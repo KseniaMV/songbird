ef0b877212ccb0606fcd5f9f68f9d3096e0a30a3
mak <mak@MacBook-Air-mak.local>
2020-08-11 12:09:36 +0300

    feat add birdData

---
+++ my-app/examples.js
@@ -0,0 +1,63 @@
+/*class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://api.example.com/items")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.items
+            });
+          },
+          // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+          // чтобы не перехватывать исключения из ошибок в самих компонентах.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Ошибка: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+      } else {
+        return (
+          <ul>
+            {items.map(item => (
+              <li key={item.name}>
+                {item.name} {item.price}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
+    {items.map(item => {
+                        return<li key={item.name}>{item}</li>
+    })}
+
+
+let response = await fetch('/article/fetch/post/user', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  body: JSON.stringify(user)
+});
+  */
--- my-app/src/components/Questions/Questions.js
+++ my-app/src/components/Questions/Questions.js
@@ -3,13 +3,73 @@ import './Questions.css';
 import BirdInfo from './BirdInfo.js';
 
 
+
 class Questions extends React.Component{
-    render(){
-        return(
-            <div className="questions-conteiner">
-                <BirdInfo/>
-            </div>
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+    componentDidMount(){
+        return new Promise((resolve,reject)=>{
+            let request = new XMLHttpRequest();
+            request.open('GET', './birdsData.js');
+            request.send();
+            let birds = request.responseText;
+            resolve(birds);
+            console.log(birds);
+
+        }).then((data)=>this.setState({
+            isLoaded: true,
+            items: data
+        })
+      
         )
+    
+        
+        /*fetch("http://localhost:3000/api/", {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json', charset: 'utf-8'
+            }
+        })
+        .then(response => response.json())
+        .then(
+            (result) => {
+                console.log(result);
+                this.setState({
+                    isLoaded: true,
+                    items: result
+                });
+            },
+            (error) =>{
+                this.setState({
+                    isLoaded: true,
+                    error
+                })
+            }
+            
+        );*/
+    }
+    render(){
+        const {error, isLoaded, items} = this.state;
+            if(error){
+                return<div>Error</div>
+            }
+            else if(isLoaded){
+                return<div>Loading...</div>
+            }else{
+                console.log({items})
+                return(
+                <div className="questions-conteiner">
+                    <BirdInfo/>
+                </div>
+                )
+            }
     }
 }
 
