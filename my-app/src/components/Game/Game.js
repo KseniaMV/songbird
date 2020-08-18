45e855325f779927634f04202131aea72cfcedec
KseniaMV <marks-83@mail.ru>
2020-08-18 13:41:43 +0300

    feat: add new action Nextbutton

--- my-app/src/components/Game/Game.js
+++ my-app/src/components/Game/Game.js
@@ -25,23 +25,23 @@ class Game extends React.Component{
      }
 
      componentDidMount(){
-         this.createQuestion();
-         this.getAnswersList();
+         this.createQuestion();    //создание вороса
+         this.getAnswersList();   //создание списка ответов для пользователя
      }
 
      //создание полей для компонента Question
      createQuestion = ()=>{
-        let randomCount = Math.floor(Math.random() * 6);
-        let category = this.state.category;
-        let currentBird = dataBirds[category][randomCount];
-        let currentBirdData = {
+        let randomCount = Math.floor(Math.random() * 6);       //случайное число от 0 - 6
+        let category = this.state.category;                    //текущая категория вопроса
+        let currentBird = dataBirds[category][randomCount];    //название птицы спрятанное в вопросе
+        let currentBirdData = {                                //данные выбранной птицы
             name: currentBird.name,
             audio: currentBird.audio,
             species: currentBird.species,
             image: currentBird.image,
             description: currentBird.description
         }
-        this.setState({
+        this.setState({                                     //запись в state загаданной птицы и ее данных
             currentBird: currentBird.name,
             bird: currentBirdData
         })
@@ -57,33 +57,32 @@ class Game extends React.Component{
 
     //получение названий птиц для списка ответов в компоненте Answers
     getAnswersList() {
-        let category = this.state.category;
-        let answersList = dataBirds[category].map((value,index)=>{
+        let category = this.state.category;                         //получение текущей категории вопросов
+        let answersList = dataBirds[category].map((value,index)=>{  //формирование списка названий птиц из текущей категории вопросов
             return value.name
         });
         this.setState({
-            answerList: answersList
+            answerList: answersList                                 //запись списка в state
         })
         return answersList;
-    }
+    };
   
     //обработка события click по варианту ответа
     handleClick = (value)=>{
         return new Promise((resolve, reject)=>{      
-            let selectedValue = value.target.value;
-            let id = value.target.id;
-            let click = this.state.clickCount;
+            let selectedValue = value.target.value;     //значение input которое выбрал пользователь
+            let id = value.target.id;                   //id выбранного input
+            let click = this.state.clickCount;          //текущее значение количества кликов пользователя
             this.setState(()=>{
                 return({
-                    answer: selectedValue,
-                    clickCount: click + 1,
+                    answer: selectedValue,              //выбранное значение запивывается в state  как овтет пользователя
+                    clickCount: click + 1,              //количество кликов пользователя увеличивается на 1
                     id: id
                 })
             });
             resolve(selectedValue);
-
             }).then(()=>this.checkAnswer());
-    }
+    };
 
     //функция сравнения ответа с вопросом
     checkAnswer = ()=>{
@@ -92,28 +91,34 @@ class Game extends React.Component{
         let currentBird = this.state.currentBird;   //название птицы, которую "загадала" игра 
         if(answer === currentBird){                 //сравнение варианта ответа и загаданной птицы
             this.scoreCount(prevScore);             //если названия равны, то вызывается функция подсчета очков, куда передается текущее количество очков пользователя
-            this.setState(()=>{
-                return({isRightAnswer: true})       //флаг = ответ правильный
-            })   
-        }
+            this.setState((state)=>{
+                return({
+                    isRightAnswer: true,            //флаг = ответ правильный
+                    count: state.count + 1
+                })      
+            }); 
+        };
 
-    }
+    };
 
     //подсчет очков
     scoreCount = (prevScore)=>{
         let answerCount = 6;                        //максимальное количество попыток  
         let clickCount = this.state.clickCount;     //количество попыток (сlick по вариантам ответов)
         let finalScore = answerCount - clickCount;  //подсчет очков в зависимости от количества уже использованных попыток 6 = 0
+        if(answerCount === 0){                      
+            finalScore = 0;
+        }  
         this.setState(()=>{
             return ({score: prevScore + finalScore}) //запись в state полученных за правильный ответ очков
         })
-    }
+    };
 
     //получить количество очков
     getScore = ()=>{
         let score = this.state.score;
         return score;
-    }
+    };
 
     //получить данные о текущей категории 
     getCategoryData=()=>{
@@ -122,14 +127,70 @@ class Game extends React.Component{
            title: Object.keys(dataBirds)
        } 
        return categoryData;
-    }
+    };
 
-    createDescription=()=>{  
-        let id = this.state.id;      //получение названия выбранной пользователем птицы
-        let category = this.state.category;     //получение категории вопросов
-        let bird = dataBirds[category][id]; //доступ к данным птицы в выбранной категории
+    //создание описания птицы
+    createDescription=()=>{                         
+        let id = this.state.id;                     //получение названия выбранной пользователем птицы
+        let category = this.state.category;         //получение категории вопросов
+        let bird = dataBirds[category][id];         //доступ к данным птицы в выбранной категории
         return bird;
-    }
+    };
+
+     //функция устанавливает категорию вопросов
+    setQuestionCategory =(categoryCount)=>{    
+        switch(categoryCount) {
+            case 1: 
+            this.setState(()=>{
+                return(
+                    {category: "Воробьиные"}
+                )
+            })
+            break
+            case 2: 
+            this.setState(()=>{
+                return(
+                    {category: "Лесные"}
+                )
+            })
+            break
+            case 3: 
+            this.setState(()=>{
+                return(
+                    {category: "Певчие птицы"}
+                )
+            })
+            break 
+            case 4: 
+            this.setState(()=>{
+                return(
+                    {category: "Хищные птицы"}
+                )
+            })
+            break 
+            case 5: 
+            this.setState(()=>{
+                return(
+                    {category: "Морские птицы"}
+                )
+            })
+            break
+            default: 
+            this.setState({category: "Разминка"});
+        };
+       
+    };
+
+    handleClickToNextLevel = (e)=>{           //клик по кнопке next level вызывает функцию установки следующей категории вопросов и устанавливает значение false флага isrightanswer
+        this.setQuestionCategory(this.state.count);
+        this.createQuestion();
+        this.getAnswersList();
+        this.setState(()=>{
+            return(
+                {isRightAnswer : false}
+            )
+        });
+    };
 
 
 
@@ -140,7 +201,7 @@ class Game extends React.Component{
                  <Menu categoryData={this.getCategoryData}/>
                  <Questions createQuestion={this.state.bird} getCurrentBird={this.getCurrentBird} isRightAnswer={this.state.isRightAnswer}/>
                  <Answers getAnswersList={this.state.answerList} handleClick={this.handleClick} isRightAnswer={this.state.isRightAnswer} createDescription={this.createDescription}/>
-                 <NextButton/>
+                 <NextButton isRightAnswer={this.state.isRightAnswer} handleClick={this.handleClickToNextLevel}/>
                 </div>
         )
     }
--- my-app/src/components/NextButton/NextButton.js
+++ my-app/src/components/NextButton/NextButton.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './NextButton.css';
 
 class NextButton extends React.Component{
-
-    handleClick = () => {
-        this.setState({
-            category: "Воробьиные"
-        })
-    }
     render(){
+        let isRightAnswer = this.props.isRightAnswer;
         return (
-            <button className="button_next-level" onClick={this.props.handleClick}>Next Level</button>
+            <button className={isRightAnswer === true? "button_next-level button--active": "button_next-level button--default"} handleclick={this.props.handleclick}>Next Level</button>
         )
     }
 }
